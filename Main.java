0f71d6cfcdd5337950f239a1beb9554a2ff58cdc
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2023-06-24 21:06:41 +0400

    architecture

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CPU cpu = new CPU(); 
+        byte i = 0;
+        cpu.RAM[0] = 99;
+        cpu.updateInstructionPointer(i);
+      
+    
+    }
+}
